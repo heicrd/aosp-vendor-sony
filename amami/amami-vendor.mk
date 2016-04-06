de3113b7bc23647c8eafe23bac613a5aeea80820
Adam Farden <adam@farden.cz>
2016-04-06 12:47:55 +0200

    SW binaries for Xperia AOSP M v04

--- amami/amami-vendor.mk
+++
@@ -1,6 +0,0 @@
-PRODUCT_PLATFORM := rhine
-CAMERA_IMX200 := true
-CAMERA_IMX132 := true
-$(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/rhine/rhine-partial.mk)
-$(call inherit-product, $(LOCAL_PATH)/amami-partial.mk)
--- castor/castor-vendor.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_PLATFORM := shinano
-CAMERA_IMX134 := true
-CAMERA_IMX132 := true
-$(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
-$(call inherit-product, vendor/sony/shinano/firmware-partial.mk)
-$(call inherit-product, vendor/sony/shinano/shinano-partial.mk)
-$(call inherit-product, $(LOCAL_PATH)/castor-partial.mk)
